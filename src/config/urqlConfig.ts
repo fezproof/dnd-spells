1299b4308c98d674e38fc6c9b2b18879027ed50e
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-17 14:55:56 +0800

    Added retry exchange to go around crashing server

--- src/config/urqlConfig.ts
+++ src/config/urqlConfig.ts
@@ -1,7 +1,19 @@
-import { createClient } from 'urql';
+import { retryExchange } from '@urql/exchange-retry';
+import {
+  createClient,
+  dedupExchange,
+  cacheExchange,
+  fetchExchange,
+} from 'urql';
 
 const client = createClient({
   url: 'https://www.dnd5eapi.co/graphql',
+  exchanges: [
+    dedupExchange,
+    cacheExchange,
+    retryExchange({ maxNumberAttempts: 5 }),
+    fetchExchange,
+  ],
 });
 
 export default client;
