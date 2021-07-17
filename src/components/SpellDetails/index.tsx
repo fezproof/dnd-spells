ed95454a94d8e5bdb56d0c23bc7c60c72fa869bd
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-17 15:48:00 +0800

    Better spell description

--- src/components/SpellDetails/index.tsx
+++ src/components/SpellDetails/index.tsx
@@ -3,10 +3,12 @@ import { FC } from 'react';
 import { SpellDetailsFragment } from '../../generated/graphql';
 
 const SpellDetails: FC<{ spell: SpellDetailsFragment }> = ({ spell }) => (
-  <div className="my-4">
-    <h4 className="text-lg">{spell.name}</h4>
+  <div className="my-4 max-w-prose">
+    <h4 className="mb-2 text-lg">{spell.name}</h4>
     {spell.desc?.map((desc, index) => (
-      <p key={index}>{desc}</p>
+      <p key={index} className="mb-2">
+        {desc}
+      </p>
     ))}
   </div>
 );
