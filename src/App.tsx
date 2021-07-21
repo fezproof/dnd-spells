d9effde9529b478f45b63d364ece81696069ee04
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-21 22:59:44 +0800

    Added title

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,11 @@ function App() {
   return (
     <div className="bg-white">
       <div className="py-16 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
-        <div className="text-center">
-          <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">
-            Welcome to
-          </h2>
-          <p className="my-3 text-4xl sm:text-5xl lg:text-6xl font-bold sm:tracking-tight text-gray-900">
-            DnD spells
-          </p>
-        </div>
+        <header className="text-center">
+          <h1 className="my-3 text-4xl sm:text-5xl lg:text-6xl font-bold sm:tracking-tight text-gray-900">
+            D&D Spellbook
+          </h1>
+        </header>
         <AppRouter />
       </div>
     </div>
