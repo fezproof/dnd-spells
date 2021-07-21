f6ac442e8ab81869d7686ce4e5963e96971c739f
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-21 22:08:08 +0800

    Spell details cleanup

--- src/components/SpellDetails/SpellDetailsInfo.tsx
+++ src/components/SpellDetails/SpellDetailsInfo.tsx
@@ -1,18 +1,26 @@
+import HourglassIcon from '@fortawesome/fontawesome-free/svgs/solid/bullseye.svg';
+import ClockIcon from '@fortawesome/fontawesome-free/svgs/solid/clock.svg';
+import HeartbeatIcon from '@fortawesome/fontawesome-free/svgs/solid/heartbeat.svg';
+import BullseyeIcon from '@fortawesome/fontawesome-free/svgs/solid/hourglass.svg';
+import ProjectIcon from '@fortawesome/fontawesome-free/svgs/solid/project-diagram.svg';
 import React, { FC } from 'react';
 import { Maybe, SpellDetailsInfoFragment } from '../../generated/graphql';
-import ClockIcon from '@fortawesome/fontawesome-free/svgs/solid/clock.svg';
-import BullseyeIcon from '@fortawesome/fontawesome-free/svgs/solid/hourglass.svg';
-import HourglassIcon from '@fortawesome/fontawesome-free/svgs/solid/bullseye.svg';
-import HeartbeatIcon from '@fortawesome/fontawesome-free/svgs/solid/heartbeat.svg';
-import ProjectIcon from '@fortawesome/fontawesome-free/svgs/solid/project-diagram.svg';
 
-const SpellInfo: FC<{ icon: string; text?: Maybe<string> }> = ({
+const SpellInfo: FC<{ icon: string; text?: Maybe<string>; type: string }> = ({
   text,
   icon,
+  type,
 }) => (
-  <div className="flex flex-col flex-nowrap justify-center items-center text-sm text-center capitalize">
-    <img src={icon} className="mb-1 w-5 h-5" />
-    {text}
+  <div className="flex flex-col flex-nowrap justify-center items-center min-w-0 text-sm text-center">
+    <img
+      src={icon}
+      className="mb-1 w-5 h-5"
+      alt={text || 'info'}
+      title={`${type}: ${text || 'info'}`}
+    />
+    <span className="overflow-hidden max-w-[8ch] sm:max-w-none overflow-ellipsis whitespace-nowrap">
+      {text}
+    </span>
   </div>
 );
 
@@ -23,27 +31,20 @@ const SpellDetailsInfo: FC<SpellDetailsInfoFragment> = ({
   concentration,
   components,
 }) => (
-  <div className="grid grid-cols-5 justify-center items-center p-3 border-t border-b border-black">
-    <SpellInfo icon={ClockIcon} text={casting_time}>
-      <img src={ClockIcon} className="w-5 h-5" />
-      {casting_time}
-    </SpellInfo>
-    <SpellInfo icon={BullseyeIcon} text={range}>
-      <img src={BullseyeIcon} className="w-5 h-5" />
-      {range}
-    </SpellInfo>
-    <SpellInfo icon={HourglassIcon} text={duration}>
-      <img src={HourglassIcon} className="w-5 h-5" />
-      {duration}
-    </SpellInfo>
-    <SpellInfo icon={HeartbeatIcon} text={concentration ? 'yes' : 'no'}>
-      <img src={HeartbeatIcon} className="w-5 h-5" />
-      {concentration ? 'yes' : 'no'}
-    </SpellInfo>
-    <SpellInfo icon={ProjectIcon} text={components?.join(' ')}>
-      <img src={ProjectIcon} className="w-5 h-5" />
-      {components}
-    </SpellInfo>
+  <div className="grid grid-cols-5 gap-2 justify-center items-center p-3 border-t border-b border-black">
+    <SpellInfo type="Cast time" icon={ClockIcon} text={casting_time} />
+    <SpellInfo type="Range" icon={BullseyeIcon} text={range} />
+    <SpellInfo type="Duration" icon={HourglassIcon} text={duration} />
+    <SpellInfo
+      type="Concentration"
+      icon={HeartbeatIcon}
+      text={concentration ? 'Yes' : 'No'}
+    />
+    <SpellInfo
+      type="Components"
+      icon={ProjectIcon}
+      text={components?.join(' ')}
+    />
   </div>
 );
 
--- src/components/SpellDetails/index.tsx
+++ src/components/SpellDetails/index.tsx
@@ -7,12 +7,12 @@ import SpellDetailsHigherLevels from './SpellDetailsHigherLevels';
 import SpellDetailsInfo from './SpellDetailsInfo';
 
 const SpellDetails: FC<{ spell: SpellDetailsFragment }> = ({ spell }) => (
-  <div className="flex flex-col flex-nowrap p-4 my-4 space-y-4 max-w-prose border border-gray-400">
+  <div className="flex flex-col flex-nowrap px-4 pt-8 pb-10 my-4 space-y-4 max-w-prose border border-gray-400">
     <SpellDetailsHeader {...spell} />
     <SpellDetailsInfo {...spell} />
     <SpellDetailsDesc {...spell} />
     <SpellDetailsBanners {...spell} />
-    <SpellDetailsHigherLevels {...spell} />
+    {!!spell.higher_level?.length && <SpellDetailsHigherLevels {...spell} />}
   </div>
 );
 
