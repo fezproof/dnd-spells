4d0968d9b1f20c68d6c597b3d423690833183ea2
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-22 20:11:56 +0800

    Changed home to a link

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="bg-white">
       <div className="py-16 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
         <header className="mb-16 text-center">
-          <Link to="/">
+          <Link to="/dnd-spells/">
             <h1 className="my-3 text-4xl sm:text-5xl lg:text-6xl font-bold sm:tracking-tight text-gray-900">
               D&D Spellbook
             </h1>
