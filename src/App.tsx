1182ddcfd22d82b4389656f74e040f575bb5d516
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-22 20:10:59 +0800

    Switch home to link

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AppRouter from './routes';
 
 function App() {
@@ -6,11 +7,11 @@ function App() {
     <div className="bg-white">
       <div className="py-16 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
         <header className="mb-16 text-center">
-          <a href="/">
+          <Link to="/">
             <h1 className="my-3 text-4xl sm:text-5xl lg:text-6xl font-bold sm:tracking-tight text-gray-900">
               D&D Spellbook
             </h1>
-          </a>
+          </Link>
         </header>
         <AppRouter />
       </div>
