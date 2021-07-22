364096306a86ca7732c6ea0438bd374cf3a0766d
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-07-22 20:22:36 +0800

    Added SPA redirection

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/dnd-spells/',
   plugins: [reactRefresh()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        redirect: resolve(__dirname, '404.html'),
+      },
+    },
+  },
 });
